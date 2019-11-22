605672f152bbdff24057eb30a398a747bfbc61e2
uns3t <uns3tzhang@gmail.com>
2019-11-22 11:05:01 +0800

    compile完成

--- kumo/Kumo.js
+++ kumo/Kumo.js
@@ -79,75 +79,161 @@ Dep.prototype={
 }
 
 //指令解析
-function Compile() {
-
+function Compile(el,vm) {
+    this.$el=this.isElementNode(el)?el:document.querySelector(el)
+    this.$vm=vm
+    if(this.$el){
+        this.$fragment=this.nodeFragment(this.$el)
+        this.compileElemet(this.$fragment)
+        this.$el.appendChild(this.$fragment)
+    }
 }
 Compile.prototype={
-    compileElemet:function () {
-
+    compileElemet:function (el) {
+        let self=this
+        let chilnode=el.childNodes;
+        [].slice().call(chilnode).forEach(node=>{
+            let text=node.textContent
+            let reg = /\{\{((?:.|\n)+?)\}\}/
+            if(self.isElementNode(node)){
+                self.compile(node)
+            }else if(self.isTextNode(node)&&reg.test(node)){
+                self.compileText(node,RegExp.$1.trim())
+            }
+            if(node.chilnode&&node.chilnode.length){
+                self.compileElemet(node)
+            }
+        })
     },
-    nodeFragment:function () {
-
+    nodeFragment:function (el) {
+        let fragment=document.createDocumentFragment()
+        let chil=null;
+        while (chil=el.firstChild){
+            fragment.appendChild(chil)
+        }
+        return fragment
     },
-    compile:function () {
+    compile:function (node) {
+        let nodeAttr=node.attributes
+        let self=this;
+        [].slice().call(nodeAttr).forEach(attr=>{
+            var attrName=attr.name
+            if(self.isDrective(attrName)){
+                var exp=attr.value
+                var dir=attrName.substring(2)
+                if(self.isEventDrective(dir)){
+                    compileUnit.eventHandler(node,self.$vm,exp,dir)
+                }else {
+                    compileUnit[dir]&&compileUnit[dir](node,self.$vm,exp)
+                }
+            node.removeAttribute(attrName)
+            }
 
+        })
     },
-    compileText:function () {
-
+    compileText:function (node,exp) {
+        compileUnit.text(node,this.$vm,exp)
     },
-    isElementNode:function () {
-
+    isElementNode:function (node) {
+        return node.nodeType===1
     },
-    isTextNode:function () {
-
+    isTextNode:function (node) {
+        return node.nodeType===3
     },
-    isDrective:function () {
-
+    isDrective:function (attr) {
+        return attr.indexOf('x-')===0
     },
-    isEventDrective:function () {
-
+    isEventDrective:function (dir) {
+        return dir.indexOf('on')===0
     }
 }
 let $elm
 let timer=null
 const compileUnit={
-    html:function () {
-
+    html:function (node,vm,exp) {
+        this.bind(node,vm,exp,'html')
     },
     text:function () {
+        this.bind(node,vm,exp,'text')
 
     },
     class:function () {
+        this.bind(node,vm,exp,'class')
 
     },
     model:function () {
+        this.bind(node,vm,exp,'model')
+
+        let self=this
+        let val=this._getVmVal(vm,exp)
+        node.addEventListener('input',function (e) {
+            let newVal=e.target.value
+            $elm=e.target
+            if(val===newVal){
+                return
+            }
+            clearTimeout(timer)
+            timer=setTimeout(function () {
+                self._setVmVal(vm,exp,newVal)
+                val=newVal
+            })
+        })
 
     },
-    bind:function () {
-
+    bind:function (node,vm,exp,dir) {
+        let updaterFn=updater[dir+'Updater']
+        updaterFn&&updaterFn(node,this._getVmVal(vm,exp))
+        new Watcher(vm,exp,function (value,oldValue) {
+            updaterFn&&updaterFn(node,value,oldValue)
+        })
     },
-    eventHandler:function () {
-
+    eventHandler:function (node,vm,exp,dir) {
+        let eventType=dir.split(':')[1]
+        let fn=vm.$options.methods&&vm.$options.methods[exp]
+        if(eventType&&fn){
+            node.addEventListener(eventType,fn.bind(vm),false)
+        }
     },
-    _getVmVal:function () {
-
+    _getVmVal:function (vm,exp) {
+        let val=vm
+        exp=exp.split('.')
+        exp.forEach(key=>{
+            key=key.trim()
+            val=val[key]
+        })
+        return val
     },
-    _setVmVal:function () {
-
+    _setVmVal:function (vm,exp,value) {
+        let val=vm
+        exp=exp.split('.')
+        exp.forEach((key,index)=>{
+            key=key.trim()
+            if(index<exp.length-1){
+                val=val[key]
+            }else {
+                val[key]=value
+            }
+        })
     }
 }
 
 const updater={
-    htmlUpdate:function () {
+    htmlUpdater:function (node,value) {
+        node.innerHTML=typeof value==='undefined'?'':value
 
     },
-    textUpdate:function () {
+    textUpdater:function (node,value) {
+        node.textContent=typeof value==='undefined'?'':value
+    },
+    classUpdater:function () {
 
     },
-    classUpdate:function () {
-
-    },
-    modelUpdate:function () {
+    modelUpdater:function (node,value,oldValue) {
+        if($elm===node){
+            return false
+        }
+        $elm=undefined
+        node.value=typeof value==='undefined'?'':value
 
     }
 }
